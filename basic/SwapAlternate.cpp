12f5242b8e90dcfdf1ae0ec82db1ca3843b03983
07komalsharma <56592343+07komalsharma@users.noreply.github.com>
2022-01-20 06:42:16 -0800

    Update SwapAlternate.cpp

--- basic/SwapAlternate.cpp
+++ basic/SwapAlternate.cpp
@@ -6,7 +6,10 @@ using namespace std;
 void swapAlt(int arr[],int size){
 	for(int i=0;i<size;i+=2){
 		if(i+1<size){
-			swap(arr[i],arr[i+1]);
+			//swap(arr[i],arr[i+1]);
+			int temp=arr[i+1];
+			arr[i+1]=arr[i];
+			arr[i]=temp;
 		}
 	}	
 }
