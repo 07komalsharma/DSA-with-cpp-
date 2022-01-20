9b340313727190f778793d295ead2df846343cbf
07komalsharma <56592343+07komalsharma@users.noreply.github.com>
2022-01-20 06:12:06 -0800

    Create SwapAlternate.cpp

---
+++ basic/SwapAlternate.cpp
@@ -0,0 +1,33 @@
+//......Swap the alternate elements of an array...
+
+#include<iostream>
+using namespace std;
+
+void swapAlt(int arr[],int size){
+	for(int i=0;i<size;i+=2){
+		if(i+1<size){
+			swap(arr[i],arr[i+1]);
+		}
+	}	
+}
+
+void printArray(int arr[],int n){
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+}
+
+int main(){
+	int even[6]={2,3,4,5,6,7};
+	int odd[5]={9,8,4,5,3};
+	
+	swapAlt(even,6);
+	swapAlt(odd,5);
+	
+	printArray(even,6);
+	printArray(odd,5);
+  
+return 0;  
+}
+
