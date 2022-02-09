0b12892138a151b02ebd078d1c4718e17bfe3f91
komal <komal.sharma220607@gmail.com>
2022-02-09 18:15:40 +0530

    solved

--- New folder/selectionsort.cpp
+++ New folder/selectionsort.cpp
@@ -1,31 +1,32 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int SelectionSort(int arr[],int n){
-	int i,minIndex;
+void SelectionSort(int arr[],int n){
+	int i,j,minIndex;
 	for(i=0;i<n-1;i++){
          minIndex=i;
-    }
-	for(int j=i+1;j<n;j++){
-		if(arr[j]<arr[minIndex]){
-			minIndex=j;
-		}
+
+	    for(j=i+1;j<n;j++){
+		   if(arr[j]<arr[minIndex])
+			   minIndex=j;
 	}
 	swap(arr[minIndex],arr[i]);
 }
+}
 
-int display(int arr[],int n){
+void display(int arr[],int n){
 	for(int i=0;i<n;i++){
 		cout<<arr[i]<<" ";
 	}
+	cout<<endl;
 }  
 
 int main(){
 	
 	int arr[6]={1,7,9,2,3,0};
 	SelectionSort(arr,6);
-	cout<<display(arr,6);
+	cout<<"sorted array:\n";
+	display(arr,6);
 	
-	
-	
+return 0;		
 }
