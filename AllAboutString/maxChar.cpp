ba995309002631aaa19d3893b71b2cd32186d376
komal <komal.sharma220607@gmail.com>
2022-02-15 12:22:37 +0530

    commit

---
+++ AllAboutString/maxChar.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+char getMaxOccnCharacter(string s){
+    int arr[26]={0};
+
+    for(int i=0;i<s.length();i++){
+        char ch=s[i];
+
+       int number=0;
+        if(ch>='a' && ch<='z'){//lowercase
+            number=ch-'a';
+    }
+        else{//uppercase
+            number=ch-'A';
+    }
+    arr[number]++;
+} 
+    int maxi=-1,ans=0;
+    for(int i=0;i<26;i++){
+        if(maxi<arr[i]){
+            ans=i;
+            maxi=arr[i];
+        }
+        maxi=max(maxi,arr[i]);
+    }
+
+    char finalAns='a'+ans;
+    return finalAns;
+}
+int main(){
+    string s;
+    cin>>s;
+    cout<<getMaxOccnCharacter(s)<<endl;
+
+
+
+
+    return 0;
+}
