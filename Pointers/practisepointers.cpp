41b032d4a0f0b4c170020e791423972ecb649a77
komal <komal.sharma220607@gmail.com>
2022-03-13 02:29:26 +0530

    commit

--- Pointers/practisepointers.cpp
+++ Pointers/practisepointers.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main(){
 
+/*
     int a=12;
     int *p=&a;
     int **q=&p;
@@ -21,5 +22,27 @@ cout<<*q<<endl;
 cout<<&(*q)<<endl;
 
 
+int a[20]={1,2,3,5};
+cout<<"->"<<&a[0]<<endl;
+
+int *p=&a[0];
+cout<<"->"<<&p<<endl;
+*/
+int arr[10];
+//error
+  //arr=arr+1;
+  
+int *ptr=&arr[0];
+cout<<ptr<<endl;
+ptr=ptr+1;
+cout<<ptr<<endl;
+
+
 return 0;
 }
+ 
+
+
+
+
+
