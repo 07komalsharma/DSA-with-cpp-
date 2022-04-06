edc53ba8d6713bf3870241e0022ca69c1798fd1b
komal <komal.sharma220607@gmail.com>
2022-04-06 22:54:03 +0530

    commit

--- Macros/macro.cpp
+++ Macros/macro.cpp
@@ -7,6 +7,8 @@ int main(){
     int r = 5;
     double area = Pi * r * r;
     cout<<"area is : "<<area<<endl;
+
+    return 0;
 }
 //koi extra storage nhi li macro define krne me koi changes nhi honge like Pi=Pi+1 whatever
 //Pi replace hoga 3.14 se 
