9c3d6924f95f6416b159cf06a995990c846c9a8c
komal <komal.sharma220607@gmail.com>
2022-04-08 21:44:47 +0530

    commit

---
+++ Recursion/challenge.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main(){
+
+int row;
+cin>>row;
+int col;
+cin>>col;
+
+int **arr=new int*[row];
+
+    for(int i=0;i<row;i++){
+        arr[i]=new int[col];
+    }
+
+
+cout<<endl;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cout<<(rand() % 10)+1<<" "; 
+        }
+        cout<<endl;
+    }
+
+}
