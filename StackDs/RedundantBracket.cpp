ffc63effdaad3450924bff28f27fbad5b0c0d143
komal <komal.sharma220607@gmail.com>
2022-08-11 16:43:19 +0530

    commit

--- StackDs/RedundantBracket.cpp
+++ StackDs/RedundantBracket.cpp
@@ -1,3 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool findRedundantBracket(string &s){
+    stack<char> st;
+
+   for(int i=0;i<s.length();i++){
+    char ch=s[i];
+
+    if(ch=='(' || ch=='-' || ch=='+' || ch =='/' || ch =='*'){
+        st.push(ch);
+    }
+    else{
+        //ch ya toh ) hai ya fir lower case letter hai
+        if(ch==')'){
+            bool isReduandant=true;
+
+            while(st.top()!='('){
+               char top =st.top();
+                if(top=='-' || top=='+' || top =='/' || top =='*'){
+                  isReduandant=false;
+                }
+                st.pop();
+            }
+            if(isReduandant==true)
+              return true;
+
+            st.pop();
+            }
+        }
+    }
+    return false;
+   } 
